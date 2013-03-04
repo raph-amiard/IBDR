05c6fcda355c21ea4ee9754270ea6521254cc2f6
planke <ludovic.gouyou.lg@gmail.com>
2013-03-04 14:23:44 +0100

    ajout sql ludo et imane

---
+++ sql/prcedures_imane.sql
@@ -0,0 +1,63 @@
+﻿/****** Object:  StoredProcedure [dbo].[inserer_client]  ******/
+-- Ajoute un nouveau client
+IF OBJECT_ID ( 'inserer_client', 'P' ) IS NOT NULL 
+    DROP PROCEDURE inserer_client;
+GO
+
+CREATE PROCEDURE [dbo].[inserer_client]
+(
+
+    @Civilite          NVARCHAR(10),
+	@Nom               NVARCHAR(64),
+	@Prenom            NVARCHAR(64),
+	@DateNaissance     DATE,
+	@Mail              NVARCHAR(128),
+	@Telephone1        NVARCHAR(20),
+	@Telephone2        NVARCHAR(20),	
+	@NumRue		   INT,
+	@TypeRue	   NVARCHAR(64),
+	@NomRue            NVARCHAR(128),
+	@ComplementAdresse NVARCHAR(256),
+	@CodePostal        NVARCHAR(10),
+	@Ville             NVARCHAR(64)
+)
+AS
+BEGIN
+	-- Insère le client
+	INSERT 	INTO CLIENT
+                   (
+			Civilite  ,        
+			Nom        ,      
+			Prenom      ,      
+			DateNaissance,     
+			Mail          ,    
+			Telephone1     ,   
+			Telephone2      , 	
+			NumRue		,	
+			TypeRue		,	
+			NomRue           , 
+			ComplementAdresse ,
+			CodePostal       ,
+			Ville             ,
+			BlackListe 
+		    )
+
+            VALUES (	
+			@Civilite          ,
+			@Nom              ,
+			@Prenom            ,
+			@DateNaissance     ,
+			@Mail              ,
+			@Telephone1        ,
+			@Telephone2       ,	
+			@NumRue		,	
+			@TypeRue	,		
+			@NomRue          ,  
+			@ComplementAdresse, 
+			@CodePostal       ,
+			@Ville             ,
+			0       
+			  )
+END
+
+GO
---
+++ sql/procedures_ludo.sql
@@ -0,0 +1,107 @@
+﻿IF OBJECT_ID ( 'niveau_relance_sur_retard', 'P' ) IS NOT NULL 
+    DROP PROCEDURE niveau_relance_sur_retard;
+GO
+
+CREATE PROCEDURE [dbo].[niveau_relance_sur_retard]
+AS 
+BEGIN
+	DECLARE @id_location INT
+	DECLARE @dateRetourPrev DATETIME
+	DECLARE Retard CURSOR FOR
+		SELECT Id, DateRetourPrev FROM Location
+		WHERE DateRetourEff is null
+	OPEN Retard
+	FETCH NEXT FROM Retard
+		INTO @id_location, @dateRetourPrev
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		IF @dateRetourPrev < CURRENT_TIMESTAMP
+		BEGIN
+			IF NOT EXISTS (	
+				SELECT * FROM RelanceRetard 
+				WHERE LocationId = @id_location
+			)
+			BEGIN
+				INSERT INTO RelanceRetard (Date, LocationId, Niveau) 
+				VALUES (CURRENT_TIMESTAMP, @id_location, 1);
+			END
+			ELSE
+			BEGIN
+				UPDATE RelanceRetard
+                SET Niveau += 1, Date = CURRENT_TIMESTAMP
+                WHERE LocationId = @id_location
+			END
+		END
+		FETCH NEXT FROM Retard
+			INTO @id_location, @dateRetourPrev
+	END
+	CLOSE Retard
+	DEALLOCATE Retard
+END
+GO
+
+
+IF OBJECT_ID ( 'relance_sur_découvert', 'P' ) IS NOT NULL 
+    DROP PROCEDURE relance_sur_découvert;
+GO
+CREATE PROCEDURE [dbo].[relance_sur_découvert] (
+	@MinSolde SMALLMONEY
+)
+AS
+BEGIN
+	DECLARE @id_abonnement INT
+	DECLARE Decouvert CURSOR FOR
+		SELECT Id FROM Abonnement
+		WHERE Solde < @MinSolde
+	OPEN Decouvert
+	FETCH NEXT FROM Decouvert
+		INTO @id_abonnement
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		IF NOT EXISTS (	
+			SELECT * FROM RelanceDecouvert 
+			WHERE AbonnementId = @id_abonnement
+		)
+		BEGIN
+			INSERT INTO RelanceDecouvert (Date, AbonnementId, Niveau) 
+			VALUES (CURRENT_TIMESTAMP, @id_abonnement, 1);
+		END
+		ELSE
+		BEGIN
+			UPDATE RelanceDecouvert
+            SET Niveau += 1, Date = CURRENT_TIMESTAMP
+            WHERE AbonnementId = @id_abonnement
+		END
+		FETCH NEXT FROM Decouvert
+			INTO @id_abonnement
+	END
+	CLOSE Decouvert
+	DEALLOCATE Decouvert 
+END 
+GO
+
+IF OBJECT_ID ( 'echeance_prochaine_abonnement', 'P' ) IS NOT NULL 
+    DROP PROCEDURE echeance_prochaine_abonnement;
+GO
+CREATE PROCEDURE [dbo].[echeance_prochaine_abonnement] (
+	@DateDiff INT
+)
+AS
+BEGIN
+	DECLARE @id_abonnement INT
+	DECLARE @dateFIN DATETIME 
+	DECLARE AbonementFin CURSOR FOR
+		SELECT Id,DateFin FROM Abonnement
+		WHERE (DateFin - CURRENT_TIMESTAMP) < @DateDiff and (DateFin - CURRENT_TIMESTAMP) > 0
+	OPEN AbonementFin
+	FETCH NEXT FROM AbonementFin
+		INTO @id_abonnement, @dateFIN
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		PRINT('Abonnement ' + CAST(@id_abonnement AS VARCHAR) + ' arrive a son terme le ' + CAST(@dateFIN AS VARCHAR))
+		FETCH NEXT FROM AbonementFin
+			INTO @id_abonnement, @dateFIN
+	END
+	CLOSE AbonementFin
+	DEALLOCATE AbonementFin
+END
